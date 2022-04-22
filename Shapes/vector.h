cdd1e7322083a74fd7c590ff2efb9df0221e5c6f
Tarfortinbor <97366047+Tarfortinbor@users.noreply.github.com>
2022-04-22 15:17:29 -0700

    Updated vector.h, still needs to be completed
    Currently missing "iterator insert", "iterator erase", and "reserve" function, put the rest of the code is made and should work for any testing

--- Shapes/vector.h
+++ Shapes/vector.h
@@ -12,47 +12,133 @@ class vector
    
   public: 
   
-   vector();     // default constructor 
+   vector() : size_v{0}, elem{nullptr}, space{0};     // default constructor 
    
-   explicit vector(int s);    // alternate constructor 
+   explicit vector(int s) : size_v{s}, elem{new vector[s]}, space{s} // alternate constructor
+    {
+        for (int i = 0; i < size_v; ++i)
+            elem[i] = 0;
+    }
    
-   vector(const vector&);    // copy constructor 
+   vector(const vector& copiedShape) : size_v{copiedShape.size_v}, elem{new vector[copiedShape.size_v]}, space{copiedShape.space} // copy constructor
+    {
+        copy(copiedShape.elem, copiedShape.elem + size_v, elem);   // copy constructor 
+    }
+
+   vector& operator=vector(const vector& copiedShape)  // copy assignment 
+   {
+        vector *p = new vector[copiedShape.size_v];       // allocate new space
+        copy(copiedShape.elem, copiedShape.elem + copiedShape.size_v, p); // copy elements - std::copy() algorithm
+        delete[] elem;                            // deallocate old space
+        elem = p;                                 // now we can reset elem
+        size_v = copiedShape.size_v;
+        return *this;  // return a self-reference
+    }
    
-   vector& operator=vector(const vector&);  // copy assignment 
+   vector(const vector&& movedShape) noexcept    // move constructor
+   {
+       size_v = movedShape.size_v;
+       elem = new vector;
+       *elem = *movedShape.elem;
+
+       delete[] movedShape.elem;
+   } 
    
-   vector(const vector&&) noexcept;    // move constructor 
+   vector& operator=vector(const vector&& movedShape) noexcept // move assignment
+   {
+       delete[] this->elem;
+       this->copy(movedShape);
+       delete[] movedShape.elem;
+       return this*;
+   } 
    
-   vector& operator=vector(const vector&&) noexcept; // move assignment 
+   ~vector()  // destructor 
+   {
+       delete[] elem;
+   }  
    
-   ~vector()      // destructor 
+   T& operator[] (int n)    // access: return reference 
+   {
+       return elem[n];
+   }
    
-   T& operator[] (int n);    // access: return reference 
+   const T& operator[] (int n)    // access: return reference 
+   {
+       return elem[n];
+   }
    
-   const T& operator[] (int n);    // access: return reference 
+   int size() const    // the current size 
+   {
+       return size_v;
+   }
    
-   int size() const;    // the current size 
+   int capacity() const     // current available space 
+   {
+       return space;
+   }
    
-   int capacity() const;     // current available space 
+   void resize(int newsize)   // grow 
+   {
+        reserve(newsize);
+        for (int i = size_v; i < newsize; ++i)
+            elem[i] = 0; // initialize new elements
+        size_v = newsize;
+    }
    
-   void resize(int newsize);   // grow 
+   void push_back(T val)   // add element 
+   {
+        if (space == 0)
+            reserve(8);         // start with space for 8 elements
+        else if (size_v == space)
+            reserve(2 * space); // get more space
+        elem[size_v] = d;       // add d at end
+        ++size_v;               // increase the size (size_v is the number of elements)
+    }
    
-   void push_back(T val);   // add element 
-   
-   void reserve(int newalloc);    // get more space  
+   void reserve(int newalloc);    // get more space 
+   {
+       return nullptr; // temp remove & replace
+   } 
    
    using iterator = T*; 
    
    using const_iterator = const T*; 
    
-   iterator begin();   // points to first element
+   iterator begin()   // points to first element
+   {
+        if (size_v == 0)
+            return nullptr;
+        return &elem[0];
+    }
    
-   const_iterator begin() const;  
+   const_iterator begin() const
+   {
+        if (size_v == 0)
+            return nullptr;
+        return &elem[0];
+    }  
    
-   iterator end();   // points to one beyond the last element 
+   iterator end()   // points to one beyond the last element 
+   {
+        if (size_v == 0)
+            return nullptr;
+        return &elem[size_v];
+    }
    
-   const_iterator end() const; 
+   const_iterator end() const
+   {
+        if (size_v == 0)
+            return nullptr;
+        return &elem[size_v];
+    } 
    
-   iterator insert(iterator p, const T& v); // insert a new element v before p 
+   iterator insert(iterator p, const T& v) // insert a new element v before p 
+   {
+       return nullptr; // temp remove & replace
+   }
    
-   iterator erase(iterator p);    // remove element pointed to by p 
+   iterator erase(iterator p)    // remove element pointed to by p 
+   {
+   return nullptr; // temp remove & replace
+   }
 }; 
