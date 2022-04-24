cc78bf5d1c23d02e9a2609cc16b54e9c7073335a
notcalamari.mp3 <71918921+imhelendang@users.noreply.github.com>
2022-04-24 10:25:22 -0700

    Update circle.h

--- Shapes/circle.h
+++ Shapes/circle.h
@@ -10,24 +10,64 @@
 #include <QPainter>
 #include <QPoint>
 #include "shape.h"
+#include <cmath>
 using namespace std;
 
 class Circle : public Shape
 {
 public:
-    // Constructor
-    Circle( QPaintDevice *pdevice, int xId, QPen xpen, QBursh xbrush );
+    /* Public data members */
+    QPen   circle_pen;
+    QBrush circle_brush;
+    QPoint top_left;
+    QPoint bottom_right;
 
-    Circle() = delete;         // default constructor 
+    /* Constructors */
+    Circle( QPaintDevice *pdevice, int xId, QPen xpen, QBrush xbrush );
+    Circle() = delete;         // default constructor
     Circle& operator=(const Circle&) = delete;  // Disallow copying
     Circle(const Circle&) = delete;
+    Circle(QPaintDevice*       pdevice,             // Constructor with parameters
+            int                assign_id,
+            QColor             assign_pen_color,
+            qreal              assign_pen_width,
+            Qt::PenStyle       assign_penstyle,
+            Qt::PenCapStyle    assign_pencap_style,
+            Qt::PenJoinStyle   assign_penjoin_style,
+            QColor             assign_burshcolor,
+            Qt::BrushStyle     assign_brushstyle,
+            int                top_leftx,
+            int                top_lefty,
+            int                assign_diameter);
+
+    /* Destrcutor */
     ~Circle() {}
 
+    /* Print function
+     * Postcondition: print out the current object ID, perimeter, and area.
+     */
     std::ostream& print(std::ostream& os) const override;
 
+    /* Sketch/draw function
+     * Postcondition: draw the shape
+     */
     void sketch(QPaintDevice* other) override;
+
+    /* Move function
+     * Postcondition: the "shape" object is moved to the desire position.
+     */
     void move(QPoint &left_side) override;
+
+    /* Update the shape function
+     * Postcondition: the object properties are updated.
+     */
+    void update(void) override;
+
+    /* Calculate perimeter or area functions
+     * Postcondition: calculate the object's perimeter and area.
+     */
     double calcPerimeter() const override;
     double calcArea() const override;
 };
 #endif // CIRCLE_H
+
