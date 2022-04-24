440561631df9cac163e7e2b23c6f79edf033635c
notcalamari.mp3 <71918921+imhelendang@users.noreply.github.com>
2022-04-24 10:26:50 -0700

    Create line.cpp

---
+++ Shapes/line.cpp
@@ -0,0 +1,72 @@
+#include "line.h"
+
+Line::Line(  QPaintDevice*      pdevice,         // Constructor with parameters
+             int                assign_id,
+             QColor             assign_pen_color,
+             qreal              assign_pen_width,
+             Qt::PenStyle       assign_penstyle,
+             Qt::PenCapStyle    assign_pencap_style,
+             Qt::PenJoinStyle   assign_penjoin_style,
+             int                top_leftx,
+             int                top_lefty,
+             int                bottom_leftx,
+             int                bottom_lefty)
+       : Shape(pdevice, assign_id, ShapeType::Line)
+{
+    line_pen.setColor(assign_pen_color);
+    line_pen.setWidth(assign_pen_width);
+    line_pen.setStyle(assign_penstyle);
+    line_pen.setCapStyle(assign_pencap_style);
+    line_pen.setJoinStyle(assign_penjoin_style);
+
+    // object specific transform from points supplied to bounding points
+    QPoint ul(top_leftx,top_lefty);
+    top_left = ul;
+    vpoints.push_back(ul);
+    QPoint lr(bottom_leftx, bottom_lefty);
+    bottom_right = lr;
+    vpoints.push_back(lr);
+}
+
+std::ostream& Line::print(std::ostream& os) const
+{
+    return os << " Id:" << GetID() << " P:" << calcPerimeter() << " A:" << calcArea();
+}
+
+void Line::sketch(QPaintDevice* other)
+{
+    QPainter& paint = GetPainter();
+    paint.begin(other);
+    paint.setPen(line_pen);
+    paint.drawLine(top_left, bottom_right);
+    paint.setPen(QPen());
+    paint.drawText((top_left.x()) - 5, (top_left.y()) - 5, QString::number(this->GetID()));
+    paint.end();
+}
+
+void Line::move(QPoint &left_side)
+{
+    int deltaX = (left_side.x() - left_side.x());
+    int deltaY = (left_side.y() - left_side.y());
+
+    top_left = left_side;
+    bottom_right.setX(bottom_right.x() + deltaX);
+    bottom_right.setY(bottom_right.y() + deltaY);
+}
+
+
+void Line::update(void)
+{
+    sketch((&GetPaintDevice()));
+    return;
+}
+
+double Line::calcPerimeter() const
+{
+    return 0;
+}
+
+double Line::calcArea() const
+{
+    return 0;
+}
