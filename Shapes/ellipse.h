9f9b80dd2c5fef804142906e4ad2c9499df356ed
notcalamari.mp3 <71918921+imhelendang@users.noreply.github.com>
2022-04-24 10:55:40 -0700

    Update ellipse.h

--- Shapes/ellipse.h
+++ Shapes/ellipse.h
@@ -15,18 +15,57 @@ using namespace std;
 class Ellipse : public Shape
 {
 public:
-    // Constructor 
-    Ellipse( QPaintDevice *pdevice, int xId, QPen xpen, QBursh xbrush );
+    /* Public data members */
+    QPen   ellipse_pen;
+    QBrush ellipse_brush;
+    QPoint top_left;
+    QPoint bottom_right;
 
-    Ellipse() = delete;         // default constructor 
+    /* Constructors */
+    Ellipse( QPaintDevice *pdevice, int xId, QPen xpen, QBrush xbrush );
+    Ellipse() = delete;         // default constructor
     Ellipse& operator=(const Ellipse&) = delete;  // Disallow copying
     Ellipse(const Ellipse&) = delete;
+    Ellipse( QPaintDevice*      pdevice,             // Constructor with parameters
+            int                assign_id,
+            QColor             assign_pen_color,
+            qreal              assign_pen_width,
+            Qt::PenStyle       assign_penstyle,
+            Qt::PenCapStyle    assign_pencap_style,
+            Qt::PenJoinStyle   assign_penjoin_style,
+            QColor             assign_burshcolor,
+            Qt::BrushStyle     assign_brushstyle,
+            int                top_leftx,
+            int                top_lefty,
+            int                assign_width,
+            int                assign_height);
+
+    /* Destrcutor */
     ~Ellipse() {}
 
+    /* Print function
+     * Postcondition: print out the current object ID, perimeter, and area.
+     */
     std::ostream& print(std::ostream& os) const override;
 
+    /* Sketch/draw function
+     * Postcondition: draw the shape
+     */
     void sketch(QPaintDevice* other) override;
+
+    /* Move function
+     * Postcondition: the "shape" object is moved to the desire position.
+     */
     void move(QPoint &left_side) override;
+
+    /* Update the shape function
+     * Postcondition: the object properties are updated.
+     */
+    void update(void) override;
+
+    /* Calculate perimeter or area functions
+     * Postcondition: calculate the object's perimeter and area.
+     */
     double calcPerimeter() const override;
     double calcArea() const override;
 };
