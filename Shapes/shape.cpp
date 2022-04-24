3e3ca22fad6484333fff8766aa69958d5cd6c9e6
notcalamari.mp3 <71918921+imhelendang@users.noreply.github.com>
2022-04-24 10:09:30 -0700

    Update shape.cpp

--- Shapes/shape.cpp
+++ Shapes/shape.cpp
@@ -1,7 +1,56 @@
 #include "shape.h"
 
-Shape( QPaintDevice *pdevice, int xId, ShapeType type, QPen xpen, QBursh xbrush )       // Copy constructor with parameters
-           : device{pDevice}, shapeId{xId}, typeShape{type}, painter{pDevice}  {
+/* Constructor with parameters */
+Shape::Shape( QPaintDevice *pdevice, int assign_id, ShapeType assign_type, QPen assign_pen, QBrush assign_brush )       // Copy constructor with parameters
+           : device{pdevice}, shapeID{assign_id}, shape_type{assign_type} /*painter{pdevice}*/  {
     pen = Qt::SolidLine;
     brush = Qt::NoBrush;
+    // painter = ??
+}
+
+/* Setters */
+void Shape::SetShapeType(ShapeType xShape) {
+    shape_type = xShape;
+}
+
+void Shape::SetPen(Qt::GlobalColor color, int width, Qt::PenStyle style, Qt::PenCapStyle pStyle, Qt::PenJoinStyle pjStyle) {
+    pen.setColor(color);
+    pen.setWidth(width);
+    pen.setStyle(style);
+    pen.setCapStyle(pStyle);
+    pen.setJoinStyle(pjStyle);
+}
+
+void Shape::SetPen(Qt::GlobalColor color) {
+    pen.setColor(color);
+}
+
+void Shape::SetBrush(Qt::GlobalColor color, Qt::BrushStyle style) {
+    brush.setColor(color);
+    brush.setStyle(style);
+}
+
+/* Getters */
+int Shape::GetID() const {
+    return shapeID;
+}
+
+QPaintDevice& Shape::GetPaintDevice() const {
+    return *device;
+}
+
+Shape::ShapeType Shape::GetShapeType() const {
+    return shape_type;
+}
+
+QPainter& Shape::GetPainter() const {
+    return *painter;
+}
+
+QPen Shape::GetPen() const {
+    return pen;
+}
+
+QBrush Shape::GetBrush() const {
+    return brush;
 }
