1fc3be77bea221bff5ed6797921e734358b30dea
notcalamari.mp3 <71918921+imhelendang@users.noreply.github.com>
2022-04-24 10:56:27 -0700

    Update main.cpp

--- Shapes/main.cpp
+++ Shapes/main.cpp
@@ -1,12 +1,22 @@
 #include "shape.h"
-#include "mainwindow.h"
 
 #include <QApplication>
+#include <QLocale>
+#include <QTranslator>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow window;
-    window.show();
+
+    QTranslator translator;
+    const QStringList uiLanguages = QLocale::system().uiLanguages();
+    for (const QString &locale : uiLanguages) {
+        const QString baseName = "2DShapes_" + QLocale(locale).name();
+        if (translator.load(":/i18n/" + baseName)) {
+            a.installTranslator(&translator);
+            break;
+        }
+    }
+    // Need implementation
     return a.exec();
 }
