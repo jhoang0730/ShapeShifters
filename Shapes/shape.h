a8addbf969eb4d6839b41a03ad4dc15646d8939a
notcalamari.mp3 <71918921+imhelendang@users.noreply.github.com>
2022-04-24 10:08:57 -0700

    Update shape.h

--- Shapes/shape.h
+++ Shapes/shape.h
@@ -1,10 +1,6 @@
 #ifndef SHAPE_H_
 #define SHAPE_H_
 
-/**************************
- *      Base Class        *
- **************************/
-
 #include <iostream>
 #include <math.h>
 using namespace std;
@@ -18,38 +14,69 @@ using namespace std;
 
 class Shape {
 public:
+    /* Public data members */
+
+
     // Shape Types
     enum class ShapeType {NoShape, Line, Polyline, Polygon, Rectangle, Square, Ellipse, Circle, Text};
 
-    // Constructors
-    Shape() = delete;         // default constructor
-
-    Shape( QPaintDevice *pdevice, int xId, ShapeType type, QPen xpen, QBursh xbrush );       // Copy constructor with parameters
-
+    /* Constructors */
+    Shape() = delete;         // Default constructor
+    Shape( QPaintDevice *pdevice, int assign_id, ShapeType assign_type) : device{pdevice}, shapeID{assign_id}, shape_type{assign_type} {}
+    Shape( QPaintDevice *pdevice, int assign_id, ShapeType assign_type, QPen assign_pen, QBrush assign_brush );       // Copy constructor with parameters
     Shape& operator=(const Shape&) = delete;  // Disallow copying
-
-    Shape(const Shape&) = delete;
+    Shape(const Shape&) = delete;             // Disallow copying
 
     // Virtual Destructor
     virtual ~Shape() {}
 
-    // Setters
+    /* Setters
+     * Precondition: ShapeType, GlobalColor, PenStyle, PenCapStyle, PenJoinStyle, BrushStyle
+     * Postcondition: set the private data members to the corresponding parameters.
+     */
     void SetShapeType(ShapeType xShape);
     void SetPen(Qt::GlobalColor, int width, Qt::PenStyle, Qt::PenCapStyle, Qt::PenJoinStyle);
     void SetPen(Qt::GlobalColor);
     void SetBrush(Qt::GlobalColor, Qt::BrushStyle);
 
-    // Getters
+    /* Getters
+     * Precondition: <none>
+     * Postcondition: return the corresponding private data members.
+     */
     int GetID() const;
+    QPaintDevice& GetPaintDevice() const;
     ShapeType GetShapeType() const;
     QPainter& GetPainter() const;
-    QPen& GetPen() const;
-    QBrush& GetBrush() const;
+    QPen GetPen() const;
+    QBrush GetBrush() const;
 
-    // Pure Virtual functions
-    virtual void sketch(QPaintDevice* other) = 0;
-    virtual void move(QPoint &left_side) = 0;
+    /* Pure Virtual functions
+     * Precondition: QPaintDevice, QPoint, ostream
+     */
+
+    /* Print function
+     * Postcondition: print out the current object ID, perimeter, and area.
+     */
     virtual std::ostream& print(std::ostream& os) const = 0;
+
+    /* Sketch/draw function
+     * Postcondition: draw the shape
+     */
+    virtual void sketch(QPaintDevice* other) = 0;
+
+    /* Move function
+     * Postcondition: the "shape" object is moved to the desire position.
+     */
+    virtual void move(QPoint &left_side) = 0;
+
+    /* Update the shape function
+     * Postcondition: the object properties are updated.
+     */
+    virtual void update(void) = 0;
+
+    /* Calculate perimeter or area functions
+     * Postcondition: calculate the object's perimeter and area.
+     */
     virtual double calcPerimeter(void) const = 0;
     virtual double calcArea(void) const = 0;
 
@@ -59,7 +86,7 @@ private:
     ShapeType shape_type;   // type of shape, from the enum shapeType
     QPen pen;
     QBrush brush;
-    QPainter *painter;      
+    QPainter *painter;
 };
 
 #endif /* SHAPE_H_ */
