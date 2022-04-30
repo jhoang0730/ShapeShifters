f9e37b27cf6785729edaa7cff51d0e899b0ef725
notcalamari.mp3 <71918921+imhelendang@users.noreply.github.com>
2022-04-29 18:09:00 -0700

    Update text.cpp

--- Shapes/text.cpp
+++ Shapes/text.cpp
@@ -1,76 +1,76 @@
-#ifndef TEXT_H_
-#define TEXT_H_
+#include"text.h"
 
-#include <iostream>
-#include <math.h>
-#include <QWidget>
-#include <QPen>
-#include <QBrush>
-#include <QFont>
-#include <QPainter>
-#include <QPoint>
-#include "shape.h"
-using namespace std;
-
-class Text : public Shape
+Text::Text(QPaintDevice*      pdevice,
+           int                assign_id,
+           QString            assign_string,
+           QColor             assign_color,
+           Qt::AlignmentFlag  assign_allignment,
+           int                assign_fontsize,
+           QString            assign_fontfamily,
+           QFont::Style       assign_fontstyle,
+           QFont::Weight      assign_fontweight,
+           int                top_leftx,
+           int                top_lefty,
+           int                assign_width,
+           int                assign_height)
+    : Shape(pdevice, assign_id, ShapeType::Text), String{assign_string}, Color{assign_color}, Alignment{assign_allignment},
+      FontSize{assign_fontsize}, FontFamily{assign_fontfamily}, FontStyle{assign_fontstyle},
+      FontWeight{assign_fontweight}
 {
-public:
-    /* Public data members */
-    QString            String;
-    QColor             Color;
-    Qt::AlignmentFlag  Alignment;
-    int                FontSize;
-    QString            FontFamily;
-    QFont::Style       FontStyle;       // normal (0), italic (1), or oblique (2)
-    QFont::Weight      FontWeight;      // thin (100), extra light (200), light (300), normal (400), medium (500), demibold (600), bold (700), extra bold (800), black (900)
-    QPoint             top_left;
-    QPoint             bottom_right;
+    /* Setting up points */
+    QPoint ul(top_leftx,top_lefty);
+    top_left = ul;
+    QPoint lr(top_leftx+assign_width, top_lefty+assign_height);
+    bottom_right = lr;
+}
 
-    /* Constructors */
-    Text() = delete;
-    Text& operator=(const Text&) = delete;  // Disallow copying
-    Text(const Text&) = delete;             // Disallow copying
-    Text(QPaintDevice*      pdevice,             // Constructor with parameters
-         int                assign_id,
-         QString            assign_string,
-         QColor             assign_color,
-         Qt::AlignmentFlag  assign_allignment,
-         int                assign_fontsize,
-         QString            assign_fontfamily,
-         QFont::Style       assign_fontstyle,
-         QFont::Weight      assign_fontweight,
-         int                top_leftx,
-         int                top_lefty,
-         int                assign_width,
-         int                assign_height);
 
-    /* Destrcutor */
-     ~Text() {}
+std::ostream& Text::print(std::ostream& os) const
+{
+    return os << "= Shape's Id:" << GetID() << " and shape's perimeter: " << calcPerimeter() << ", area: " << calcArea();
+}
 
-    /* Print function
-     * Postcondition: print out the current object ID, perimeter, and area.
-     */
-    std::ostream& print(std::ostream& os) const override;
 
-    /* Sketch/draw function
-     * Postcondition: draw the shape
-     */
-    void sketch(QPaintDevice* other) override;
+void Text::sketch(QPaintDevice* other)
+{
+    QPainter& tPaint = GetPainter();
+    QFont tFont(FontFamily, FontSize);
+    QPen  tPen(Color);
+    QRect tRect(top_left, bottom_right);
+    tFont.setStyle(FontStyle);
+    tFont.setWeight(FontWeight);
+    tPaint.begin(&GetPaintDevice());
+    tPaint.setFont(tFont);
+    tPaint.setPen(tPen);
+    tPaint.drawText(tRect, Alignment, String);
+    tPaint.setPen(QPen());
+    tPaint.drawText((top_left.x()) - 5, (top_left.y()) - 5, QString::number(this->GetID()));
+    tPaint.end();
+}
 
-    /* Move function
-     * Postcondition: the "shape" object is moved to the desire position.
-     */
-    void move(QPoint &left_side) override;
 
-    /* Update the shape function
-     * Postcondition: the object properties are updated.
-     */
-    void update(void) override;
+void Text::move(QPoint &left_side)
+{
+    top_left = left_side;
+    bottom_right.setX(bottom_right.x() + left_side.x() - top_left.x());
+    bottom_right.setY(bottom_right.y() + left_side.y() - top_left.y());
+}
 
-    /* Calculate perimeter or area functions
-     * Postcondition: calculate the object's perimeter and area.
-     */
-    double calcPerimeter() const override;
-    double calcArea() const override;
-};
-#endif // TEXT_H_
+
+void Text::update(void)
+{
+    sketch((&GetPaintDevice()));
+    return;
+}
+
+
+double Text::calcPerimeter() const
+{
+    return 0;
+}
+
+
+double Text::calcArea() const
+{
+    return 0;
+}
