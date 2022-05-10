aa7c5a2767501c055cffcc255ccb0924e68aa333
Tarfortinbor <97366047+Tarfortinbor@users.noreply.github.com>
2022-05-10 15:29:11 -0700

    Update mainwindow.cpp
    Button implementation, doesn't do anything but the framework is set up for further implementation

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -13,3 +13,39 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::shape_choice()
+{
+    if(ui->circleChoice->isEnabled())
+    {
+
+    }
+    if(ui->squareChoice->isEnabled())
+    {
+
+    }
+    if(ui->rectangleChoice->isEnabled())
+    {
+
+    }
+    if(ui->lineChoice->isEnabled())
+    {
+
+    }
+    if(ui->polygonChoice->isEnabled())
+    {
+
+    }
+    if(ui->polylineChoice->isEnabled())
+    {
+
+    }
+    if(ui->ellipseChoice->isEnabled())
+    {
+
+    }
+    if(ui->textChoice->isEnabled())
+    {
+
+    }
+
+}
