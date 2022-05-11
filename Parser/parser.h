af216f482fbdca8eb213b1f679ea6b534de80412
Rainbonium <66752661+Rainbonium@users.noreply.github.com>
2022-05-11 13:27:29 -0700

    Master (#3)
    * Added almost blank shape.cpp file to test commit.
    
    * Signed-off-by: Rainbonium <gabeaxel127@gmail.com>
    
    * Useless file.

---
+++ Parser/parser.h
@@ -0,0 +1,51 @@
+#ifndef PARSER_H
+#define PARSER_H
+
+using namespace std;
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include "shape.h"
+#include "line.h"
+#include "polyline.h"
+#include "polygon.h"
+#include "rectangle.h"
+#include "square.h"
+#include "ellipse.h"
+#include "circle.h"
+#include "text.h"
+#include "vector.h"
+
+enum ShapeNames
+{
+	LINE = 1,		// Subsequent emurators recieve the value of the previous one +1.
+	POLYLINE,
+	POLYGON,
+	RECTANGLE,
+	SQUARE,
+	ELLIPSE,
+	CIRCLE,
+	TEXT
+}
+
+Shape* readLine(ifstream&, int id);
+Shape* readPolyLine(ifstream&, int id);
+Shape* readPolygon(ifstream&, int id);
+Shape* readRectangle(ifstream&, int id);
+Shape* readSquare(ifstream&, int id);
+Shape* readEllipse(ifstream&, int id);
+Shape* readCircle(ifstream&, int id);
+Shape* readText(ifstream&, int id);
+
+Qt::GlobalColor getColor(const string&);
+Qt::PenStyle getPen(const string&);
+Qt::PenCapStyle getCap(const string&);
+Qt::PenJoinStyle getJoin(const string&);
+Qt::BrushStyle getBrush(const string&);
+Qt::AlignmentFlag getAlignFlag(const string&);
+QFont::Style getText(const string&)
+QFont::Weight getWeight(const string&);
+
+#endif // Shape_Parser
