4554558f679b4faf3dbd316740ef7dbb239354f6
HaeroM <89627948+HaeroM@users.noreply.github.com>
2022-05-17 18:49:05 -0700

    Added loginscreen and Contact us
    - modified loginscreen.ui and loginscreen.cpp
    -new reasource file for universal access to logo

--- LoginScreen/loginscreen.cpp
+++ LoginScreen/loginscreen.cpp
@@ -1,18 +1,29 @@
 #include "loginscreen.h"
 #include "ui_loginscreen.h"
+#include <QPixmap>
 
+
+//Constructor
 LoginScreen::LoginScreen(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::LoginScreen)
 {
     ui->setupUi(this);
+
+    //Logo Logic
+    QPixmap pix(":/resources/Logo.jpg");
+    ui->pic->setPixmap(pix.scaledToHeight(100).scaledToWidth(100));
+
+
 }
 
+//Desturctor
 LoginScreen::~LoginScreen()
 {
     delete ui;
 }
 
+//Button
 void LoginScreen::on_pushButton_clicked()
 {
     QString username = ui->lineEdit_Username->text();
